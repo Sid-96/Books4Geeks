20b378ef2f991464ae53a680a91e68bf98c5b164
Siddharthsinh Rana <sidsinh2014@gmail.com>
2016-12-22 15:44:02 +0530

    Create Volley Singleton class

---
+++ app/src/main/java/com/b4g/sid/books4geeks/VolleySingleton.java
@@ -0,0 +1,42 @@
+package com.b4g.sid.books4geeks;
+
+import android.graphics.Bitmap;
+import android.util.LruCache;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by Sid on 22-Dec-16.
+ */
+
+public class VolleySingleton {
+
+    public static VolleySingleton instance;
+
+    public static VolleySingleton getInstance() {
+        if(instance==null)  instance = new VolleySingleton();
+        return instance;
+    }
+
+    public RequestQueue requestQueue;
+    public ImageLoader imageLoader;
+
+    // Constructor
+    private VolleySingleton() {
+        requestQueue = Volley.newRequestQueue(B4GAppClass.getAppContext());
+        imageLoader = new ImageLoader(requestQueue, new ImageLoader.ImageCache() {
+            private final LruCache<String, Bitmap> cache = new LruCache<>(20);
+            @Override
+            public Bitmap getBitmap(String url) {
+                return cache.get(url);
+            }
+            @Override
+            public void putBitmap(String url, Bitmap bitmap) {
+                cache.put(url, bitmap);
+            }
+        });
+    }
+}
+
