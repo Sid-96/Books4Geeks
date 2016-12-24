1ca1179a2af0b3e2d076a6a6ec541fef28fee6ba
Siddharthsinh Rana <sidsinh2014@gmail.com>
2016-12-24 12:37:20 +0530

    Create Util class for accessing API

---
+++ app/src/main/java/com/b4g/sid/books4geeks/Util/ApiUtil.java
@@ -0,0 +1,27 @@
+package com.b4g.sid.books4geeks.Util;
+
+import com.b4g.sid.books4geeks.B4GAppClass;
+import com.b4g.sid.books4geeks.R;
+
+/**
+ * Created by Sid on 24-Dec-16.
+ */
+
+public class ApiUtil {
+
+    private ApiUtil(){
+
+    }
+
+    private static String getGoogleBooksApiKey(){
+        return B4GAppClass.getAppContext().getResources().getString(R.string.google_books_api_key);
+    }
+
+    private static String getNytApiKey(){
+        return B4GAppClass.getAppContext().getResources().getString(R.string.nyt_api_key);
+    }
+
+    public static String getBestSellerList(String listName){
+        return "https://api.nytimes.com/svc/books/v3/lists//"+listName+".json?api-key=" + getNytApiKey();
+    }
+}
