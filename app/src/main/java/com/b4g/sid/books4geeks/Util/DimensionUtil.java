ac2021e68192fa97575a3ecad05b89d38d1f6bef
Siddharthsinh Rana <sidsinh2014@gmail.com>
2016-12-30 11:29:41 +0530

    Add boolean for checking if device is tablet

--- app/src/main/java/com/b4g/sid/books4geeks/Util/DimensionUtil.java
+++ app/src/main/java/com/b4g/sid/books4geeks/Util/DimensionUtil.java
@@ -3,6 +3,7 @@ package com.b4g.sid.books4geeks.Util;
 import android.util.DisplayMetrics;
 
 import com.b4g.sid.books4geeks.B4GAppClass;
+import com.b4g.sid.books4geeks.R;
 
 /**
  * Created by Sid on 25-Dec-16.
@@ -12,6 +13,10 @@ public class DimensionUtil {
 
     private DimensionUtil(){
 
+    }
+
+    public static boolean isTablet(){
+        return B4GAppClass.getAppContext().getResources().getBoolean(R.bool.is_tablet);
     }
 
     public static int getNumberOfColumns(int dimenId, int minColumns){
