66d5f6edc259b8a5239c6706c236677afe5f1161
Siddharthsinh Rana <sidsinh2014@gmail.com>
2017-01-03 14:19:23 +0530

    Add content provider to manifest

--- app/src/main/java/com/b4g/sid/books4geeks/data/BookProvider.java
+++ app/src/main/java/com/b4g/sid/books4geeks/data/BookProvider.java
@@ -2,6 +2,7 @@ package com.b4g.sid.books4geeks.data;
 
 import android.net.Uri;
 
+import net.simonvt.schematic.annotation.ContentProvider;
 import net.simonvt.schematic.annotation.ContentUri;
 import net.simonvt.schematic.annotation.TableEndpoint;
 
@@ -9,6 +10,7 @@ import net.simonvt.schematic.annotation.TableEndpoint;
  * Created by Sid on 30-Dec-16.
  */
 
+@ContentProvider(authority = BookProvider.AUTHORITY, database  = BookDB.class)
 public class BookProvider {
 
     public static final String AUTHORITY = "com.b4g.sid.books4geeks.data.BookProvider";
