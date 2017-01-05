ca26ab65f6a2edd5dee2da0159cbfb3805d41322
Siddharthsinh Rana <sidsinh2014@gmail.com>
2017-01-05 18:32:09 +0530

    Complete SettingsActivity

---
+++ app/src/main/java/com/b4g/sid/books4geeks/ui/activity/SettingsActivity.java
@@ -0,0 +1,34 @@
+package com.b4g.sid.books4geeks.ui.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import com.b4g.sid.books4geeks.R;
+import com.b4g.sid.books4geeks.ui.Fragment.SettingsFragment;
+
+public class SettingsActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
+
+        getFragmentManager().beginTransaction().replace(R.id.settings_layout,new SettingsFragment()).commit();
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()== android.R.id.home){
+            onBackPressed();
+            return true;
+        }
+        return false;
+    }
+}
